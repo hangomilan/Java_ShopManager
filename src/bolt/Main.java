4e6340bde4e0a6550f5ec3610fc01bf4ea4a85b2
hangomilan <hmilancodecool@gmail.com>
2016-09-15 13:23:43 +0200

    Add some tests and main

--- src/bolt/Main.java
+++ src/bolt/Main.java
@@ -1,9 +1,39 @@
 package bolt;
 
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+
+import bolt.aruk.Szappan;
+import bolt.aruk.Tej;
+import bolt.aruk.tej.TartosTej;
+import bolt.kivetel.ZarvaKivetel;
+
 public class Main {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) 
+	{
+		TartosTej tej1 = new TartosTej(321312332L, Tej.LITER, "tej gyarto", new GregorianCalendar(2018,11,11), Tej.ZSIROS);
+		TartosTej tej2 = new TartosTej(321312332L, Tej.LITER, "tej gyarto", new GregorianCalendar(2018,11,11), Tej.FELZSIROS);
+		Szappan szappan1 = new Szappan(321344121L, "szappan gyarto A", 'A');
+		Szappan szappan2 = new Szappan(366644121L, "szappan gyarto B", 'B');
+
+		System.out.println(tej1.joMeg());
+		System.out.println(tej2.getUrtartalom());
+		System.out.println(szappan1.getMosohatas());
+		System.out.println(szappan2.vonalKod);
+		
+		Hashtable<Long, Integer> aruk = new Hashtable<>();
+		aruk.put(tej1.vonalKod, 5);
+		aruk.put(tej2.vonalKod, 10);
+		aruk.put(szappan1.vonalKod, 15);
+		aruk.put(szappan2.vonalKod, 20);
+		
+		Bolt bolt = new Bolt("Bolt 1", "Tulaj 1", "Utca 1", aruk, 1);
+		try {
+			System.out.println(bolt.aruk());
+		} catch (ZarvaKivetel e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -3,25 +3,35 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.util.GregorianCalendar;
+import java.util.Hashtable;
 
 import org.junit.Test;
 
+import bolt.Bolt;
 import bolt.aruk.Sajt;
+import bolt.aruk.Szappan;
 import bolt.aruk.Tej;
 import bolt.aruk.tej.TartosTej;
 
 public class Tests {
 	
 	@Test
-	public void tartostejJomeg()
+	public void zsirosTejJomeg()
 	{
-		Tej tej = new TartosTej(12345678910L, Tej.LITER, "gyarto",  new GregorianCalendar(2017, 04, 11), Tej.ZSIROS);
+		Tej tej = new TartosTej(12345678910L, Tej.LITER, "tej gyarto",  new GregorianCalendar(2017, 04, 11), Tej.ZSIROS);
 		Boolean result = tej.joMeg();
 		assertEquals(true, result);
 	}
 	
 	@Test
+	public void felzsirosTejJomeg()
+	{
+		Tej tej = new TartosTej(12345678910L, Tej.LITER, "tej gyarto",  new GregorianCalendar(2017, 06, 21), Tej.FELZSIROS);
+		Boolean result = tej.joMeg();
+		assertEquals(true, result);
+	}
 	
+	@Test
 	public void sajtjJomeg()
 	{
 		Sajt sajt = new Sajt(12345678910L, 1000, "sajt gyarto", new GregorianCalendar(2017, 11, 11), true);
@@ -29,4 +39,32 @@ public class Tests {
 		assertEquals(true, result);
 	}
 	
+	@Test
+	public void sajtSulyNemNull()
+	{
+		Sajt sajt = new Sajt(23123123123L, 2500, "sajt gyarto", new GregorianCalendar(2016, 12, 12), false);
+		assertNotNull(sajt);
+	}
+
+	@Test
+	public void szappanMosohatasNemNullA()
+	{
+		Szappan szappan = new Szappan(9876543211L, "szappan gyarto", 'A');
+		assertNotNull(szappan);
+	}
+	
+	@Test
+	public void szappanMosohatasNemNullB()
+	{
+		Szappan szappan = new Szappan(9876543211L, "szappan gyarto", 'B');
+		assertNotNull(szappan);
+	}
+	
+	@Test
+	public void ujBoltLetrehozasNemNull()
+	{
+		Hashtable<Long, Integer> aruk = new Hashtable<>();
+		Bolt ujBolt = new Bolt("Uj bolt", "Uj tulaj", "Uj cim", aruk, 1);
+		assertNotNull("Letrejött az uj bolt.", ujBolt);
+	}
 }
